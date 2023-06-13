ac42c314fc6f0be435b1e605ff306c9d5e7e9f8d
Abderrazak <abderrazakbouasker@gmail.com>
2023-06-13 17:04:13 +0100

    user modification forms wip

--- src/Form/ModalUser/ModifyEmailType.php
+++ src/Form/ModalUser/ModifyEmailType.php
@@ -1,40 +1,28 @@
 <?php
 
-namespace App\Form;
+namespace App\Form\ModalUser;
 
-use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
 
-class UserType extends AbstractType
+class ModifyEmailType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username')
-//            ->add('roles')
-            ->add('password')
             ->add('email')
-//            ->add('rank')
             ->add('PlainPassword', PasswordType::class, [
                 'required' => true,
             ])
-//            ->add('currentPassword', PasswordType::class, [
-//                'constraints' => [
-//                    new UserPassword(),
-//                ],
-//            ])
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            //'data_class' => User::class,
-            'data_class' => null,
+            // Configure your form options here
         ]);
     }
 }
